0a27b2d0ce48670b100789bc25d29b5b827c5380
gtoborges <aaborges@tjgo.gov>
2018-06-29 10:49:41 -0300

    estruturas de controle - exemplos

---
+++ controle/if2.js
@@ -0,0 +1,10 @@
+function teste1(num){
+    if(num > 7)
+        console.log(num)
+        console.log('Final')
+}
+
+//sem o '{}', só sera executada a primeira linha de comando.
+
+teste1(6)
+teste1(8)
